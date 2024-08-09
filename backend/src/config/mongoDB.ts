bee668e00e449953e741def0c035a54517dc922f
Arad Ben Menashe <aradbm@gmail.com>
2024-08-09 15:53:56 +0300

    Update mongoDB configuration

--- backend/src/config/mongoDB.ts
+++ backend/src/config/mongoDB.ts
@@ -1,13 +1,16 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const mongoUrl = 'mongodb://localhost:27017/testdb';
+const MONGOURL = process.env.MONGOURL || "mongodb://localhost:27017/testdb";
 
 export const connectToMongoDB = async (): Promise<void> => {
   try {
-    await mongoose.connect(mongoUrl);
-    console.log('Connected to MongoDB');
+    await mongoose.connect(MONGOURL);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } catch (error) {
-    console.error('MongoDB connection error:', error);
+    console.error("MongoDB connection error:", error);
     process.exit(1);
   }
 };
